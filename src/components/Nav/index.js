407d69eded968610fe2a49a708d7847557516cf4
anthonycc <anthonyccardozo@gmail.com>
2021-10-14 20:32:02 -0400

    fix nav tests, add tests for gallery&photolist

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,30 +4,26 @@
 
 
 
-import React, {useEffect} from "react";
+import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
 
-const {
-  categories = [],
-  setCurrentCategory,
-  currentCategory,
-} = props;
-
-useEffect(() => {
-  document.title = capitalizeFirstLetter(currentCategory.name);
-}, [currentCategory]);
-
-  // const handleClick = () => {
-  //   console.log("click handled")
-  // }
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
@@ -38,22 +34,22 @@ useEffect(() => {
             </a>
           </li>
           <li className={"mx-2"}>
-            <span>
-              Contact
-            </span>
+            <span onClick={() => handleClick("Contact")}>Contact</span>
           </li>
-          {
-            categories.map((category) => (
-              <li className= {`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
-                key={category.name} >
-                <span
-                onCLick={() => {setCurrentCategory(category)}}
-                >
-                 {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))
-          }
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${currentCategory.name === category.name}`}
+              key={category.name}
+            >
+              <span
+                onCLick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
@@ -61,68 +57,3 @@ useEffect(() => {
 }
 
 export default Nav;
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import { capitalizeFirstLetter } from "../../utils/helpers";
-
-// function Nav() {
-// const [currentCategory, setCurrentCategory] = useState(categories[0]);
-// const [categories] = useState([
-//   {
-//     name: 'commercial',
-//     description: 'Photos of grocery stores, food trucks, and other commercial projects',
-//   },
-//   { name: 'portraits', description: 'Portraits of people in my life' },
-//   { name: 'food', description: 'Delicious delicacies' },
-//   { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-// ]);
-
-//   return (
-//     <header className="flex-row px-1">
-//       <h2>
-//         <a data-testid="link" href="/">
-//           <span role="img" aria-label="camera">
-//             {" "}
-//             📸
-//           </span>{" "}
-//           Oh Snap!
-//         </a>
-//       </h2>
-//       <nav>
-//         <ul className="flex-row">
-//           <li className="mx-2">
-//             <a
-//               href="#about"
-//             >
-//               About me
-//             </a>
-//           </li>
-//           <li>
-//             <span>Contact</span>
-//           </li>
-//           {categories.map((category) => (
-//             <li className={`mx-1 ${
-//                 currentCategory.name === category.name && 'navActive'
-//                 }`} key={category.name}>
-//               <span
-//                 onClick={() => {
-//                   setCurrentCategory(category)
-//                 }}
-//               >
-//                 {capitalizeFirstLetter(category.name)}
-//               </span>
-//             </li>
-//           ))}
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// }
-
-// export default Nav;
