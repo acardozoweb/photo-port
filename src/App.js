b6199cf8646146a77cc97d968ae3620c0e4ce8e0
anthonycc <anthonyccardozo@gmail.com>
2021-10-15 13:09:20 -0400

    making it a SPA

--- src/App.js
+++ src/App.js
@@ -9,13 +9,21 @@ import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
-    { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects",},
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
     { name: "portraits", description: "Portraits of people in my life" },
     { name: "food", description: "Delicious delicacies" },
-    { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature", },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -23,13 +31,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-          <ContactForm></ContactForm>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
